0115a5994a0a2210c6fd5b4a7c46fc952e5ce62b
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-04-19 19:44:52 +0300

    first commit

---
+++ src/NewComponent.tsx
@@ -0,0 +1,22 @@
+type NewComponentPropsType = {
+    students: Array<StudentsPropsType>
+}
+
+type StudentsPropsType = {
+    id: number,
+    name: string,
+    age: number
+}
+
+export const NewComponent = (props: NewComponentPropsType) => {
+    return (
+        <ul>
+            {props.students.map((s, index) => {
+
+                return (
+                    <li key={s.id}>{s.name}, {s.age}</li>
+                )
+            })}
+        </ul>
+    )
+}
