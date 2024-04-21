b5d4607226356a962f60af5d035fb8b47d797ea8
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-04-21 12:37:32 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import {Map} from './map/Map';
 import {ButtonOnClick} from './button/ButtonOnClick';
 import {Buttons} from './button/ButtonCallback/Buttons';
 import {UseState} from './useState/useState';
+import {Filter} from './filter/Filter';
 
 function App() {
     return (
-        <UseState/>
+        <Filter/>
     );
 }
 
