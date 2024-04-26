1852580985f49fb277117f49b35ab6bb7f1542d6
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-04-26 15:46:54 +0300

    test 01

--- src/NativeJavaScript_TypeScript/testing/01.test.ts
+++ src/NativeJavaScript_TypeScript/testing/01.test.ts
@@ -1,4 +1,4 @@
-import {mult, sum} from './01';
+import {mult, splitIntoWords, sum} from './01';
 
 let a: number;
 let b: number;
@@ -26,4 +26,15 @@ test('mult should be correct', () => {
 
     //expect result(ожидаемый результат)
     expect(result).toBe(5);
+})
+test('split into words should be correct', () => {
+    //data
+    const sent = 'Hello words!'
+    //action(действие)
+    const result = splitIntoWords(sent);
+
+    //expect result(ожидаемый результат)
+    expect(result.length).toBe(2);
+    expect(result[0]).toBe('hello');
+    expect(result[1]).toBe('words');
 })
--- src/NativeJavaScript_TypeScript/testing/01.ts
+++ src/NativeJavaScript_TypeScript/testing/01.ts
@@ -5,3 +5,10 @@ export const sum = (a: number, b: number) => {
 export const mult = (a: number, b: number) => {
     return a * b
 }
+
+export const splitIntoWords = (sentence: string) => {
+    return sentence
+        .toLowerCase()
+        .split(' ')
+        .map(w => w.replace('!', ''))
+}
