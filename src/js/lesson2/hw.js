d7a3525063bac1a5ea7e2292b61b2ae084646b0b
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-05-06 19:55:12 +0300

    array methods, polyfills

--- src/js/lesson2/hw.js
+++ src/js/lesson2/hw.js
@@ -1,4 +1,4 @@
-let fruits = ['apple', 'banana', 'orange'];
+/*let fruits = ['apple', 'banana', 'orange'];
 fruits.push('lime', 'apple')
 console.log(fruits); // Output: ['apple', 'banana', 'orange', твои добавленные элементы в конец]
 
@@ -88,6 +88,75 @@ let people = [
 ];
 
 console.log(sortAlphabeticallyByProperty(people, 'name')); // [ { name: 'Alice', age: 25 },{ name: 'Bob', age: 35 },{ name: 'John', age: 30 }]
+
+function sortDescending (arr) {
+    return arr.sort((a, b) => b-a)
+}
+let numberss = [5, 2, 8, 1, 4];
+console.log(sortDescending(numberss)); // Output: [8, 5, 4, 2, 1]*/
+
+
+let data = [
+    {value: 1, name: 'Option 1'},
+    {value: 2, name: 'Option 2'},
+    {value: 3, name: 'Option 3'},
+];
+
+const formattedData = data.map((el) => {
+    return {value: el.value, label: el.name}
+})
+console.log(formattedData); // [
+//   { value: 1, label: 'Option 1' },
+// { value: 2, label: 'Option 2' },
+// { value: 3, label: 'Option 3' }
+// ]
+
+let products = [
+    {id: 1, name: 'Apple', price: 1.99},
+    {id: 2, name: 'Banana', price: 0.99},
+    {id: 3, name: 'Orange', price: 2.49},
+];
+let productsWithDiscount = products.map((el) => {
+    return {...el, discountedPrice: el.price * 0.9}
+})
+console.log(productsWithDiscount); // [
+//   { id: 1, name: 'Apple', price: 1.99, с: 1.791 },
+//   { id: 2, name: 'Banana', price: 0.99, discountedPrice: 0.891 },
+//   { id: 3, name: 'Orange', price: 2.49, discountedPrice: 2.241 }
+// ]
+
+function isPalindrome(str) {
+    /*debugger*/
+    let one = str.split('').reverse().join('')
+    return one === str
+}
+
+console.log(isPalindrome('level')); // Вернет true
+console.log(isPalindrome('racecar')); // Вернет true
+console.log(isPalindrome('hello')); // Вернет false
+
+let arr = [1, 1, 5, 6, 7, 8, 9, 9, 3, 4, 0, 19];
+
+function removeDublicates(arr) {
+    return arr.filter((el, index) => arr.indexOf(el) !== index)
+}
+
+console.log(removeDublicates(arr));//[ 1, 9 ]
+
+
+const persons = [
+    {name: 'Alice', age: 30},
+    {name: 'Bob', age: 25},
+    {name: 'Charlie', age: 35},
+];
+
+function sortByProperty(arr, property) {
+    return arr.sort((a, b) => a[property] - b[property])
+}
+
+console.log(sortByProperty(persons, 'age'));
+// [{ name: 'Bob', age: 25 }, { name: 'Alice', age: 30 }, { name: 'Charlie', age: 35 }]
+
 
 
 
