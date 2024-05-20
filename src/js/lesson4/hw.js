2c5810518b90c4a9be00a128b08f4a993e82c6a4
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-05-20 20:23:15 +0300

    Map Set

--- src/js/lesson4/hw.js
+++ src/js/lesson4/hw.js
@@ -61,7 +61,7 @@ console.log(student1);  // 'Alex'
 console.log(student3);  // 'Chris'*/
 
 
-const person = { firstName: 'Max', lastName: 'Johnson' };
+/*const person = { firstName: 'Max', lastName: 'Johnson' };
 
 const firstName = 'first Name: Nina';
 const lastName = 'last Name: Ivanov';
@@ -70,14 +70,71 @@ const {firstName: one, lastName: two, age = 25} = person
 
 console.log(one); // 'Max'
 console.log(two); // 'Johnson'
-console.log(age); // 25 (значение по умолчанию)
+console.log(age); // 25 (значение по умолчанию)*/
+
+/*let student = {
+    name: 'John',
+    age: 20,
+    course: 'Math',
+};
+
+const propertyCount = Object.keys(student).length
+
+console.log(propertyCount); // 3*/
 
 
+/*let scores = {
+    math: 90,
+    english: 85,
+    science: 92,
+};
+
+const totalScore = Object.values(scores).reduce((acc, item) => acc + item)
+
+console.log(totalScore); // 267*/
 
 
+/*let obj = { a: 1, b: 2, c: 3 };
+function objectToArray(obj){
+    return Object.entries(obj)
+}
+console.log(objectToArray(obj)); // [['a', 1], ['b', 2], ['c', 3]]*/
+
+/*const studentGrades = new Map()
+studentGrades.set('1', 'one')
+studentGrades.set('1', 'one')
+studentGrades.set('Bob', 85)
+console.log(studentGrades.get('Bob')); // 85*/
 
 
+/*let products = new Map([
+    ['Milk', 1.2],
+    ['Bread', 1.5],
+    ['Cheese', 2.5],
+]);
+if(products.has('Bread')){
+    products.delete('Bread')
+}
 
 
+console.log(products.has('Bread')); // false
+console.log(products); // Map(2) { 'Milk' => 1.2, 'Cheese' => 2.5 }*/
 
+
+/*let fruits = new Set(['apple', 'banana', 'mango']);
+if(fruits.has('banana')){
+    fruits.delete('banana')
+}
+
+console.log(fruits.has('banana')); // false
+console.log(fruits); // Set(2) { 'apple', 'mango' }*/
+
+
+const numbers = [1, 1, 4, 6, 7, 8, 9, 0, 0, 10, 10, 11]
+const result = new Set(numbers)
+
+console.log('result: ', result); //result:  [
+//   1, 4,  6,  7, 8,
+//   9, 0, 10, 11
+// ]
 
