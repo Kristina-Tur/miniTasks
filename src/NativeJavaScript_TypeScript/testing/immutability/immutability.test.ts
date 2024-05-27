b83bb944d757687bdd67de488ea505c51d48769b
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-05-27 22:33:54 +0300

    immutability

--- src/NativeJavaScript_TypeScript/testing/immutability/immutability.test.ts
+++ src/NativeJavaScript_TypeScript/testing/immutability/immutability.test.ts
@@ -1,13 +1,136 @@
-import {increaseAge, UserType} from "./immutability";
+import {
+    moveBooks,
+    moveLaptop,
+    moveUser,
+    removeBook,
+    updateBooks,
+    updateCompanies,
+    UserType,
+    UserTypeAndBook, UserTypeAndCompanies
+} from "./immutability";
 
-test("Reference type test", () => {
+test("upgrade address", () => {
     let user: UserType = {
-        name: "Daniil",
-        age: 22,
-        address: { title: "Minsk" },
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        }
     };
 
-    increaseAge(user);
+    const movedUser = moveUser(user, 'Kiev');
 
-    expect(user.age).toBe(23);
+    expect(user).not.toBe(movedUser);
+    expect(user.address).not.toBe(movedUser.address);
+    expect(user.laptop).toBe(movedUser.laptop);
+    expect(movedUser.address.title).toBe('Kiev')
 });
+
+test("upgrade laptop", () => {
+    let user: UserType = {
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        }
+    };
+
+    const movedLaptop = moveLaptop(user, 'Asus');
+
+    expect(user).not.toBe(movedLaptop);
+    expect(user.laptop).not.toBe(movedLaptop.laptop);
+    expect(user.address).toBe(movedLaptop.address);
+    expect(movedLaptop.laptop.title).toBe('Asus')
+});
+test("add new book", () => {
+    let user: UserTypeAndBook = {
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        },
+        book: ['html', 'css', 'js']
+    };
+
+    const movedBooks = moveBooks(user, 'ts');
+
+    expect(user).not.toBe(movedBooks);
+    expect(user.laptop).toBe(movedBooks.laptop);
+    expect(user.address).toBe(movedBooks.address);
+    expect(user.book).not.toBe(movedBooks.book);
+    expect(movedBooks.book[3]).toBe('ts')
+    expect(user.book[2]).toBe('js')
+});
+test("update books", () => {
+    let user: UserTypeAndBook = {
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        },
+        book: ['html', 'css', 'js']
+    };
+
+    const movedBooks = updateBooks(user, 'js',  'ts');
+
+    expect(user).not.toBe(movedBooks);
+    expect(user.laptop).toBe(movedBooks.laptop);
+    expect(user.address).toBe(movedBooks.address);
+    expect(user.book).not.toBe(movedBooks.book);
+    expect(movedBooks.book[2]).toBe('ts')
+});
+test("remove book", () => {
+    let user: UserTypeAndBook = {
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        },
+        book: ['html', 'css', 'js']
+    };
+
+    const movedBooks = removeBook(user, 'js');
+
+    expect(user).not.toBe(movedBooks);
+    expect(user.laptop).toBe(movedBooks.laptop);
+    expect(user.address).toBe(movedBooks.address);
+    expect(user.book).not.toBe(movedBooks.book);
+    expect(movedBooks.book[2]).toBe(undefined);
+    expect(movedBooks.book.length).toBe(2)
+});
+
+test("update companies", () => {
+    let user: UserTypeAndCompanies = {
+        name: "Kristina",
+        age: 28,
+        address: {
+            title: "Brest"
+        },
+        laptop: {
+            title: 'Honor'
+        },
+        book: ['html', 'css', 'js'],
+        companies: [{id: 1, title: 'Епам'}, {id: 2, title: 'Google'}]
+    };
+
+    const movedBooks = updateCompanies(user, 1, 'Epam');
+
+    expect(user).not.toBe(movedBooks);
+    expect(movedBooks.companies[0].title).toBe('Epam')
+});
+
--- src/NativeJavaScript_TypeScript/testing/immutability/immutability.ts
+++ src/NativeJavaScript_TypeScript/testing/immutability/immutability.ts
@@ -1,9 +1,45 @@
 export type UserType = {
     name: string;
     age: number;
-    address: { title: string };
+    address: {
+        title: string
+    };
+    laptop: {
+        title: string
+    }
 };
 
-export function increaseAge(u: UserType) {
-    u.age++;
+export type UserTypeAndBook = UserType & {
+    book: string[]
+}
+
+type Companies = {
+    id: number
+    title: string
+}
+
+export type UserTypeAndCompanies = UserTypeAndBook & {
+    companies: Companies[]
+}
+
+
+export function moveUser(u: UserType, title: string) {
+    return {...u, address: {title: title}}
+}
+
+export function moveLaptop(u: UserType, title: string) {
+    return {...u, laptop: {title: title}}
+}
+
+export function moveBooks(u: UserTypeAndBook, title: string) {
+    return {...u, book: [...u.book, title]}
+}
+export function updateBooks(u: UserTypeAndBook, title: string, title2: string) {
+    return {...u, book: u.book.map(item => item === title ? title2 : item)}
+}
+export function removeBook(u: UserTypeAndBook, title: string) {
+    return {...u, book: u.book.filter(b => b !== title )}
+}
+export function updateCompanies(u: UserTypeAndCompanies, index: number, title: string) {
+    return {...u, companies: u.companies.map(c => c.id === index ? {...c, title} : c)}
 }
