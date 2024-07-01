6917795aaf197f6ec61f39f8a971416bed89a1ba
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-07-01 19:49:54 +0300

    promise

--- src/js/lesson6/js6.js
+++ src/js/lesson6/js6.js
@@ -133,49 +133,47 @@
 
 // })
 
-// const promiseFetch = (url) => {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       res("some data from server");
-//       // rej("some error");
-//     }, 1000);
-//   });
-// };
+/* const promiseFetch = (url) => {
+   return new Promise((res, rej) => {
+     setTimeout(() => {
+       res("some data from server");
+       // rej("some error");
+     }, 1000);
+   });
+ };
 
-// // import promiseFetch from 'blabla'
-
-// promiseFetch("google.com")
-//   .then((data) => {
-//     console.log("then1", data);
-//     return 10;
-//   })
-//   .then((data) => {
-//     console.log("then2", data);
-//     foo();
-//     // return data; // throw new Error("a is not defined");
-//   })
-//   .then((data) => {
-//     console.log("then3", data);
-//   })
-//   .catch((err) => {
-//     console.log("catch1", err.message);
-//     // foo2();
-//     return 50;
-//   })
-//   .then((data) => {
-//     console.log("then4", data);
-//     return 100;
-//   })
-//   .catch((err) => {
-//     console.log("catch2", err.message);
-//   })
-//   .finally((data) => {
-//     console.log("finally", data); //100
-//     return 200;
-//   })
-//   .then((data) => {
-//     console.log("then5", data);
-//   });
+ promiseFetch("google.com")
+   .then((data) => {
+     console.log("then1", data);
+     return 10;
+   })
+   .then((data) => {
+     console.log("then2", data);
+     foo();
+     // return data; // throw new Error("a is not defined");
+   })
+   .then((data) => {
+     console.log("then3", data);
+   })
+   .catch((err) => {
+     console.log("catch1", err.message);
+     // foo2();
+     return 50;
+   })
+   .then((data) => {
+     console.log("then4", data);
+     return 100;
+   })
+   .catch((err) => {
+     console.log("catch2", err.message);
+   })
+   .finally((data) => {
+     console.log("finally", data); //100
+     return 200;
+   })
+   .then((data) => {
+     console.log("then5", data);
+   });*/
 
 // const promiseFetch = (url) => {
 //   return new Promise((res, rej) => {
@@ -207,18 +205,86 @@
 
 // delay(2000).then(() => console.log("Hello!"));
 
-const promiseFetch = (url) => {
+/*const promiseFetch = (url) => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       // res("some data from server");
       rej("rejected1");
     }, 3000);
-  });
+   });
 };
 
 promiseFetch()
-  .catch((t) => t + "catch1") //"rejected1"
+  .catch((t) => t + "catch1")
   .catch((t) => t + "catch2")
-  .then((t) => t + "then1") // "rejected1catch1"
+  .then((t) => t + "then1")
   .finally((t) => t + "finally")
-  .then((t) => console.log(t)); // "rejected1catch1then1"
+  .then((t) => console.log(t));*/
+
+/*/!*Создание промиса: Напишите функцию createPromise, которая принимает число и возвращает промис. Промис должен ждать
+количество миллисекунд, равное переданному числу, и затем возвращать это число.*!/
+const createPromises = (number) => {
+    return new Promise((res) => {
+        setTimeout(() => {
+            res(number)
+        }, number)
+    })
+}
+createPromises(3000)
+    .then((number) => {
+        console.log(number)
+    })*/
+
+
+/*/!*Использование .then и .catch: Напишите функцию handlePromise, которая принимает промис и использует .then для вывода
+результата промиса и .catch для вывода ошибки, если промис отклонен.*!/
+const promise = new Promise((res, rej) => {
+    res(10)
+    rej('Error')
+})
+
+const handlePromise = (promise) => {
+    return promise
+        .then((data) => {
+            console.log(data)
+            return data
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+console.log(handlePromise(promise))*/
+
+/*/!*
+Использование .finally: Напишите функцию executePromise, которая принимает промис и использует .finally для вывода
+сообщения о том, что промис завершен, независимо от того, выполнен он успешно или отклонен.
+*!/
+
+const executePromise = (promise) => {
+     return promise
+        .then((data) => {return data})
+        .catch(() => {return 'Error'})
+        .finally(() => {
+            console.log('Promise is done')})
+}
+console.log(executePromise(Promise.resolve('success')))*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
